66a005260cea612da1548252936720c433702ddc
Ashley Bonaro <adbonaro0@frostburg.edu>
2018-02-01 17:04:11 -0500

    Add files via upload

---
+++ Username.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cosc240_project0_adbonaro0;
+
+/**
+ *
+ * @author Mowsie2
+ */
+public class Username {
+    public static void main(String[] args){
+        System.out.println("  /\\  |--\\ |--\\ /--\\ |\\   |");
+        System.out.println(" /__\\ |   ||--<|    ||  \\ |");
+        System.out.println("/    \\|__/ |__/ \\__/ |   \\|");
+    }    
+}
